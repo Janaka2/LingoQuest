acf742c4ea50b7f1b89bb478a30c339dbe57ed14
janaka2@gmail.com <Magegithub@#_3>
2023-06-04 17:33:54 +0200

    working with pagination

--- App.tsx
+++ App.tsx
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
-import { SafeAreaView, View, Text, Button, StyleSheet, useColorScheme, FlatList } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, View, Text, Button, StyleSheet, useColorScheme, FlatList, ActivityIndicator } from 'react-native';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 import DATA from './data';
+
+const PAGE_SIZE = 20; // Number of items to load in each batch
 
 type DataType = {
   id: string;
@@ -35,8 +37,9 @@ const Item = ({ item, increment, decrement, done, setDone }: ItemProps) => {
 
   return (
     <View style={styles.item}>
-      <Text style={styles.title}>{item.german}</Text>
-      <Text style={styles.subtitle}>{item.english}</Text>
+      <Text style={styles.title}>Question {item.id}</Text>
+      <Text style={styles.german}>{item.german}</Text>
+      <Text style={styles.english}>{item.english}</Text>
       <View style={styles.buttonsContainer}>
         <Button onPress={handleCorrect} title="Correct" color="#4CAF50" />
         <Button onPress={handleWrong} title="Wrong" color="#F44336" />
@@ -55,41 +58,73 @@ const Header = ({ points, resetPoints }: { points: number; resetPoints: () => vo
 const LingoQuest = () => {
   const [points, setPoints] = useState(0);
   const [doneSet, setDoneSet] = useState(new Set<string>());
+  const [data, setData] = useState<DataType[]>([]); // Data to be rendered
+  const [page, setPage] = useState(0); // Current page
+  const [loading, setLoading] = useState(true); // Loading state
+
+  useEffect(() => {
+    const loadPageData = () => {
+      // Simulate loading delay
+      setLoading(true);
+      setTimeout(() => {
+        const start = page * PAGE_SIZE;
+        const end = start + PAGE_SIZE;
+        setData((prevData) => [...prevData, ...DATA.slice(start, end)]);
+        setLoading(false);
+      }, 1000);
+    };
+
+    loadPageData();
+  }, [page]);
 
   const handleCorrect = () => {
-    setPoints(points + 10);
+    setPoints((prevPoints) => prevPoints + 10);
   };
 
   const handleWrong = () => {
-    setPoints(points - 10);
+    setPoints((prevPoints) => prevPoints - 10);
   };
 
   const handleDone = (id: string) => {
-    setDoneSet(new Set(doneSet.add(id)));
+    setDoneSet((prevDoneSet) => new Set(prevDoneSet.add(id)));
   };
 
   const resetPoints = () => {
     setPoints(0);
+    setPage(0);
     setDoneSet(new Set<string>());
+    setData([]);
   };
 
-  const renderItem = ({ item }: { item: DataType }) => (
-    <Item item={item} increment={handleCorrect} decrement={handleWrong} done={doneSet.has(item.id)} setDone={handleDone} />
-  );
+  const nextPage = () => {
+    setPage((prevPage) => prevPage + 1);
+  };
 
   const isDarkMode = useColorScheme() === 'dark';
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+  const renderItem = ({ item }: { item: DataType }) => (
+    <Item item={item} increment={handleCorrect} decrement={handleWrong} done={doneSet.has(item.id)} setDone={handleDone} />
+  );
+
+  const renderFooter = () => {
+    if (!loading) return null;
+    return <ActivityIndicator style={styles.loadingIndicator} size="large" color="#3F51B5" />;
+  };
+
   return (
     <SafeAreaView style={backgroundStyle}>
       <Header points={points} resetPoints={resetPoints} />
       <FlatList
-        data={DATA}
+        data={data}
         renderItem={renderItem}
-        keyExtractor={item => item.id}
+        keyExtractor={(item) => item.id}
         contentContainerStyle={styles.container}
+        ListFooterComponent={renderFooter}
+        onEndReached={nextPage}
+        onEndReachedThreshold={0.5}
       />
     </SafeAreaView>
   );
@@ -97,6 +132,7 @@ const LingoQuest = () => {
 
 const styles = StyleSheet.create({
   container: {
+    flexGrow: 1,
     marginTop: 20,
   },
   item: {
@@ -112,10 +148,14 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginBottom: 10,
   },
-  subtitle: {
-    fontSize: 20,
+  german: {
+    fontSize: 18,
     color: '#3F51B5',
-    marginBottom: 20,
+    marginBottom: 5,
+  },
+  english: {
+    fontSize: 16,
+    color: '#3F51B5',
   },
   buttonsContainer: {
     flexDirection: 'row',
@@ -145,6 +185,9 @@ const styles = StyleSheet.create({
   doneText: {
     fontSize: 16,
     color: '#3F51B5',
+  },
+  loadingIndicator: {
+    marginVertical: 20,
   },
 });
 
